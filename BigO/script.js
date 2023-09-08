8be41349a002dd9fcddc1ece9566cf18d9e7b82b
Arashk <arashkyoosha@gmail.com>
2023-09-07 21:16:36 -0400

    added folders

---
+++ BigO/script.js
@@ -0,0 +1,17 @@
+const nemo = ["nemo"];
+const everyone = ["dory", "bruce", "marlin", "nemo", "gill", "bloat", "nigel", "squirt", "darla", "hank"];
+const large = new Array(100).fill("nemo");
+
+
+function findNemo(array) {
+    let t0 = performance.now();
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === "nemo") {
+            console.log("We found Nemo!");
+        }
+    }
+    let t1 = performance.now();
+    console.log(t1 - t0);
+}
+
+findNemo(large);
