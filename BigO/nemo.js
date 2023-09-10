b83ada4074b7f7d5f09daa56ff13ca70908f9f2e
Arashk <arashkyoosha@gmail.com>
2023-09-09 21:26:48 -0400

    Changing JS file name

--- BigO/nemo.js
+++ BigO/nemo.js
@@ -4,14 +4,15 @@ const large = new Array(100).fill("nemo");
 
 
 function findNemo(array) {
-    let t0 = performance.now();
+    //let t0 = performance.now();
     for (let i = 0; i < array.length; i++) {
         if (array[i] === "nemo") {
             console.log("We found Nemo!");
         }
     }
-    let t1 = performance.now();
-    console.log(t1 - t0);
+    //let t1 = performance.now();
+    //console.log(t1 - t0);
 }
 
 findNemo(large);
+
