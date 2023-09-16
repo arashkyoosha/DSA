79a557f23045356f6101b2d8a110eda7b474b3d8
Arashk <arashkyoosha@gmail.com>
2023-09-16 12:20:25 -0400

    changes to JS file

--- BigO/nemo.js
+++ BigO/nemo.js
@@ -1,24 +1,36 @@
 const nemo = ["nemo"]; // O(1) --> Constant Time
-const everyone = ["dory", "bruce", "marlin", "nemo", "gill", "bloat", "nigel", "squirt", "darla", "hank"]; // O(n)
+const everyone = [
+  "dory",
+  "bruce",
+  "marlin",
+  "gill",
+  "bloat",
+  "nigel",
+  "squirt",
+  "darla",
+  "hank",
+  "nemo",
+]; // O(n)
 const large = new Array(100).fill("nemo"); // O(n)
 
-
 function findNemo(array) {
-    //let t0 = performance.now();
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === "nemo") {
-            console.log("We found Nemo!");
-        }
+  //let t0 = performance.now();
+  for (let i = 0; i < array.length; i++) {
+    console.log("running");
+    if (array[i] === "nemo") {
+      console.log("We found Nemo!");
+      break;
     }
-    //let t1 = performance.now();
-    //console.log(t1 - t0);
+  }
+  //let t1 = performance.now();
+  //console.log(t1 - t0);
 }
 
-findNemo(large);
-
+findNemo(everyone);
 
 // O(n) --> Linear Time
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // BIG O EXERCISES
 
@@ -27,7 +39,8 @@ function funChallenge(input) {
   let a = 10; // O(1)
   a = 50 + 3; // O(1)
 
-  for (let i = 0; i < input.length; i++) { // O(n)
+  for (let i = 0; i < input.length; i++) {
+    // O(n)
     anotherFunction(); // O(n)
     let stranger = true; // O(n)
     a++; // O(n)
@@ -39,25 +52,98 @@ function funChallenge(input) {
 // So:
 // O(n)
 
-
 // What is the Big O of the below function? (Hint, you may want to go line by line)
 function anotherFunChallenge(input) {
   let a = 5; // O(1)
   let b = 10; // O(1)
   let c = 50; // O(1)
-  for (let i = 0; i < input; i++) { // O(n)
+  for (let i = 0; i < input; i++) {
+    // O(n)
     let x = i + 1; // O(n)
     let y = i + 2; // O(n)
     let z = i + 3; // O(n)
   }
-  for (let j = 0; j < input; j++) { // O(n)
+  for (let j = 0; j < input; j++) {
+    // O(n)
     let p = j * 2; // O(n)
     let q = j * 2; // O(n)
   }
   let whoAmI = "I don't know"; // O(1)
 }
 
-
 // O(4 + 5n(or 7n))
 // So:
 // O(n)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// BIG O EXERCISES
+
+function printFirstItemThenFirstHalfThenSayHi100Times(items) {
+  console.log(items[0]);
+
+  var middleIndex = Math.floor(items.length / 2);
+  var index = 0;
+
+  while (index < middleIndex) {
+    console.log(items[index]);
+    index++;
+  }
+
+  for (var i = 0; i < 100; i++) {
+    console.log("hi");
+  }
+}
+
+// O(1 + n/2 + 100)
+// becomes:
+// O(n)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// function compressBoxesTwice(boxes, boxes2) {
+//   boxes.foEach(function (boxes) {
+//     console.log(boxes);
+//   });
+
+//   boxes2.foEach(function (boxes) {
+//     console.log(boxes);
+//   });
+// }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Log all pairs of array
+
+const boxes = ["a", "b", "c", "d", "e"];
+
+function logAllPairsOfArray(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      console.log(boxes[i], boxes[j]);
+    }
+  }
+}
+
+logAllPairsOfArray(boxes);
+
+// O( n * n)
+// becomes: O(n^2)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function printAllNumbersThenAllPairSums(numbers) {
+  console.log("these are the numbers:");
+  numbers.forEach(function (number) {
+    console.log(number);
+  });
+
+  console.log("and these are their sums:");
+  numbers.forEach(function (firstNumber) {
+    numbers.forEach(function (secondNumber) {
+      console.log(firstNumber + secondNumber);
+    });
+  });
+}
+
+printAllNumbersThenAllPairSums([1, 2, 3, 4, 5]);
