2bec84d802f69fee6bcfefd995dcb2fd910de4a5
Arashk <arashkyoosha@gmail.com>
2023-09-16 20:42:43 -0400

    changes to JS file

--- BigO/nemo.js
+++ BigO/nemo.js
@@ -147,3 +147,26 @@ function printAllNumbersThenAllPairSums(numbers) {
 }
 
 printAllNumbersThenAllPairSums([1, 2, 3, 4, 5]);
+
+// O(n + n^2)
+// Becomes: O(n^2)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//#5 Space complexity O(1)
+function boooo(n) {
+  for (let i = 0; i < n; i++) {
+    console.log("booooo");
+  }
+}
+
+// #6 Space complexity O(n)
+function arrayOfHiNTimes(n) {
+  var hiArray = [];
+  for (let i = 0; i < n; i++) {
+    hiArray[i] = "hi";
+  }
+  return hiArray;
+}
+
+arrayOfHiNTimes(6);
